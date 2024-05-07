bdf3fe2284a91b59b3d155cccd9389ee923a8641
muhammadghufran2222 <ghufranlogicsaint@gmail.com>
2024-05-07 15:54:03 +0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,56 @@
+// Replace text in header
+// const checkReplace = document.querySelector('.replace-me');
+
+
+// if(checkReplace !== null) {
+//     const replace = new  ReplaceME(checkReplace, {
+//         animation: 'animated fadeIn',
+//         speed: 2000,
+//         separator:',',
+//         loopCount: 'infinite',
+//         autoRun: true,
+//     });
+// }
+ 
+
+// Replace text in header
+const checkReplace = document.querySelector('.replace-me');
+
+if (checkReplace !== null) {
+    const words = ['Small Business', 'Startups', 'Enterprise'];
+    let index = 0;
+
+    function animateText() {
+        checkReplace.style.opacity = 0;
+        setTimeout(() => {
+            checkReplace.textContent = words[index];
+            checkReplace.style.opacity = 1;
+            index = (index + 1) % words.length;
+        }, 500); 
+    }
+    animateText();
+    setInterval(animateText, 2000); 
+}
+
+
+
+// User Scroll For Navbar
+function userScroll() {
+    const navbar = document.querySelector('.navbar');
+  
+    window.addEventListener('scroll', () => {
+      if (window.scrollY > 50) {
+        navbar.classList.add('bg-dark');
+        navbar.classList.add('border-bottom');
+        navbar.classList.add('border-secondary');
+        navbar.classList.add('navbar-sticky');
+      } else {
+        navbar.classList.remove('bg-dark');
+        navbar.classList.remove('border-bottom');
+        navbar.classList.remove('border-secondary');
+        navbar.classList.remove('navbar-sticky');
+      }
+    });
+  }
+  
+  document.addEventListener('DOMContentLoaded', userScroll);
